be0cb30ad2bf82f7bcc85d40bc52721e23dcdd66
sifiso <simabuz022@student.wethinkcode.co.za>
2023-06-19 11:35:43 +0200

    fixed json error and able to connect to most websites

--- scraper.py
+++ scraper.py
@@ -1,5 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
+def getJSON():
+    try:
+        with open("website.json") as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print("JSON file not found")
+        
+def getBanksJSON(data):
+    try:
+        return data["banks"]
+    except TypeError:
+        print("Incorrect data type!")
 
+def getAllPersonalAccounts(data):
 
+    try:
+        for bank in data:
+            response = requests.get(bank["urls"]["home"])
+            print(response.status_code)
+    except:
+        print("error")
+
+if __name__ == "__main__":
+    js = getJSON()
+    banks = getBanksJSON(js)
+    # print(banks)
+    getAllPersonalAccounts(banks)
